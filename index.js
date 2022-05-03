f70248557dd11ecbd0aea9904a29ef0df6914186
Dipankar <dipankar15-11137@diu.edu.bd>
2022-05-04 03:16:46 +0600

    remove item

--- index.js
+++ index.js
@@ -44,6 +44,14 @@ async function run() {
             const result = await productsCollection.insertOne(newProduct);
             res.send(result);
         });
+
+        // Delete
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productsCollection.deleteOne(query);
+            res.send(result);
+        })
     }
     finally { }
 }
